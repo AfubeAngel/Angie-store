13522825265df1135a930a26d67a2e22ba85654a
Angel Afube <angel@zestpayment.com>
2023-12-07 11:09:46 +0100

    add the dashboard page

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -11,26 +11,21 @@ import {
 import { formatCurrency } from './utils';
 
 export async function fetchRevenue() {
-  // Add noStore() here prevent the response from being cached.
-  // This is equivalent to in fetch(..., {cache: 'no-store'}).
 
   try {
-    // Artificially delay a reponse for demo purposes.
-    // Don't do this in real life :)
-
     // console.log('Fetching revenue data...');
     // await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch complete after 3 seconds.');
+    console.log('Data fetch complete after 3 seconds.');
 
     return data.rows;
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch revenue data.');
   }
-}
+} 
 
 export async function fetchLatestInvoices() {
   try {
