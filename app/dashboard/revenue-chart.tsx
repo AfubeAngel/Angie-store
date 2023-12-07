a02d05610702f4cb4353534640c602151afd5307
Angel Afube <angel@zestpayment.com>
2023-12-07 11:56:38 +0100

    include the invoice segment on the dashboard page

--- app/dashboard/revenue-chart.tsx
+++ app/dashboard/revenue-chart.tsx
@@ -3,19 +3,12 @@ import { CalendarIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 import { Revenue } from '@/app/lib/definitions';
 
-// This component is representational only.
-// For data visualization UI, check out:
-// https://www.tremor.so/
-// https://www.chartjs.org/
-// https://airbnb.io/visx/
-
 export default async function RevenueChart({
   revenue,
 }: {
   revenue: Revenue[];
 }) {
   const chartHeight = 350;
-  // NOTE: comment in this code when you get to this point in the course
 
   const { yAxisLabels, topLabel } = generateYAxis(revenue);
 
@@ -23,26 +16,27 @@ export default async function RevenueChart({
     return <p className="mt-4 text-gray-400">No data available.</p>;
   }
 
+  const barWidth = 100 / revenue.length; // Calculate the width based on the number of bars
+  const spacing = 5; // Set the spacing between bars
+
   return (
     <div className="w-full md:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Recent Revenue
       </h2>
-      {/* NOTE: comment in this code when you get to this point in the course */}
 
-      { <div className="rounded-xl bg-gray-50 p-4">
-        <div className="sm:grid-cols-13 mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-white p-4 md:gap-4">
-          <div
-            className="mb-6 hidden flex-col justify-between text-sm text-gray-400 sm:flex"
-            style={{ height: `${chartHeight}px` }}
-          >
-            {yAxisLabels.map((label) => (
-              <p key={label}>{label}</p>
-            ))}
-          </div>
-
-          {revenue.map((month) => (
-            <div key={month.month} className="flex flex-col items-center gap-2">
+      <div className="rounded-xl bg-gray-50 p-4">
+        <div className="flex items-end">
+          {revenue.map((month, index) => (
+            <div
+              key={month.month}
+              className="flex flex-col items-center gap-3"
+              style={{
+                flex: `1 0 calc(${barWidth}% - ${spacing}px)`, // Subtract spacing
+                maxWidth: `calc(${barWidth}% - ${spacing}px)`, // Subtract spacing
+                marginRight: index < revenue.length - 1 ? `${spacing}px` : '0', // Add margin-right except for the last bar
+              }}
+            >
               <div
                 className="w-full rounded-md bg-blue-300"
                 style={{
@@ -59,7 +53,7 @@ export default async function RevenueChart({
           <CalendarIcon className="h-5 w-5 text-gray-500" />
           <h3 className="ml-2 text-sm text-gray-500 ">Last 12 months</h3>
         </div>
-      </div> }
+      </div>
     </div>
   );
 }
